a62b8a5a9b8cac6a5aa2def9a7de57da9372572f
mafiefa02 <10221006@mahasiswa.itb.ac.id>
2024-04-27 22:24:10 +0700

    chore: group and change theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,13 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -24,16 +24,35 @@ const config = {
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
+        neutral: {
+          DEFAULT: "hsl(var(--neutral))",
+          100: "hsl(var(--neutral-100))",
+          300: "hsl(var(--neutral-300))",
+          foreground: "hsl(var(--neutral-foreground))",
+        },
         primary: {
           DEFAULT: "hsl(var(--primary))",
+          50: "hsl(var(--primary-50))",
+          300: "hsl(var(--primary-300))",
+          500: "hsl(var(--primary-500))",
+          700: "hsl(var(--primary-700))",
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
+          50: "hsl(var(--secondary-50))",
+          300: "hsl(var(--secondary-300))",
+          500: "hsl(var(--secondary-500))",
+          700: "hsl(var(--secondary-700))",
+          900: "hsl(var(--secondary-900))",
           foreground: "hsl(var(--secondary-foreground))",
         },
         destructive: {
           DEFAULT: "hsl(var(--destructive))",
+          100: "hsl(var(--destructive-100))",
+          400: "hsl(var(--destructive-400))",
+          800: "hsl(var(--destructive-800))",
+          950: "hsl(var(--destructive-950))",
           foreground: "hsl(var(--destructive-foreground))",
         },
         muted: {
@@ -42,6 +61,10 @@ const config = {
         },
         accent: {
           DEFAULT: "hsl(var(--accent))",
+          100: "hsl(var(--accent-100))",
+          400: "hsl(var(--accent-400))",
+          800: "hsl(var(--accent-800))",
+          950: "hsl(var(--accent-950))",
           foreground: "hsl(var(--accent-foreground))",
         },
         popover: {
@@ -51,6 +74,22 @@ const config = {
         card: {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
+        },
+        success: {
+          DEFAULT: "hsl(var(--success))",
+          50: "hsl(var(--success-50))",
+          500: "hsl(var(--success-500))",
+          700: "hsl(var(--success-700))",
+          900: "hsl(var(--success-900))",
+          foreground: "hsl(var(--success-foreground))",
+        },
+        warning: {
+          DEFAULT: "hsl(var(--warning))",
+          50: "hsl(var(--warning-50))",
+          300: "hsl(var(--warning-300))",
+          700: "hsl(var(--warning-700))",
+          900: "hsl(var(--warning-900))",
+          foreground: "hsl(var(--warning-foreground))",
         },
       },
       borderRadius: {
@@ -75,6 +114,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
