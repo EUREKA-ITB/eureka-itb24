84f7bc17e89c1700737aee58162773644b1ccfb5
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2024-05-15 22:58:35 +0700

    chore: delete draft landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-import Navbar from "../components/navbar";
-import Onboarding from "../components/onboarding";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center">
-      <Navbar />
-      <Onboarding />
-      <p className="pb-4 text-gray-500">Copyright © 2024 EUREKA! ITB 2024</p>
+    <main className="flex size-full h-screen items-center justify-center gap-8">
+      Ini diisi sama coming soon
     </main>
   );
 }
