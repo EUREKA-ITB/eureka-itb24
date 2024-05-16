b68416bc4ac751c8cf786a9b19fec0032e0110ba
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2024-05-16 18:32:35 +0700

    chore: minor adjustment
    - removed react fragmet from footer
    - removed src/app/reference/onboarding in include array of tsconfig.json

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,76 +5,74 @@ import { BsFacebook, BsInstagram, BsTwitterX } from "react-icons/bs";
 
 export default function Footer() {
   return (
-    <>
-      <footer className="w-full px-20 pb-10">
-        <div className="flex flex-col justify-between  md:flex-row md:items-center ">
-          <div className="flex flex-col items-center">
-            <Image
-              src={logo_eureka}
-              width={300}
-              alt=""
-            />
-          </div>
-          <div className="flex flex-col items-end space-y-2 pt-10 md:pt-0 ">
-            <p>The Physics Multiverse </p>
-            <h1 className="text-4xl font-bold italic">EUREKA! ITB 2024</h1>
-            <h2 className="pt-10 text-2xl font-bold">COMPETITIONS</h2>
-            <Link
-              href="#"
-              className="hover:underline"
-            >
-              Physics Olympiad
-            </Link>
-            <Link
-              href="#"
-              className="hover:underline"
-            >
-              Scientific Writing Competition
-            </Link>
-            <h2 className="pt-10 text-2xl font-bold">ACKNOWLEDGEMENTS</h2>
-            <Link
-              href="#"
-              className="hover:underline"
-            >
-              Sponsors
-            </Link>
-            <Link
-              href="#"
-              className="hover:underline"
-            >
-              Media Partners
-            </Link>
-          </div>
+    <footer className="w-full px-20 pb-10">
+      <div className="flex flex-col justify-between  md:flex-row md:items-center ">
+        <div className="flex flex-col items-center">
+          <Image
+            src={logo_eureka}
+            width={300}
+            alt=""
+          />
         </div>
-        <div>
-          <div className="pt-5 sm:flex sm:items-center sm:justify-between">
-            <div className="flex">
-              <Link
-                href="https://www.instagram.com/eurekaitb/"
-                className="px-3 text-gray-400 hover:text-gray-200"
-              >
-                <BsInstagram />
-              </Link>
-              <div className="icon-instagram"></div>
-              <Link
-                href="https://twitter.com/eurekaitb"
-                className="px-3 text-gray-400 hover:text-gray-200"
-              >
-                <BsTwitterX />
-              </Link>
-              <Link
-                href="https://www.facebook.com/EurekaITB/"
-                className="px-3 text-gray-400 hover:text-gray-200"
-              >
-                <BsFacebook />
-              </Link>
-            </div>
-            <span className="text-sm text-gray-400 sm:text-center">
-              © 2024 EUREKA! ITB
-            </span>
-          </div>
+        <div className="flex flex-col items-end space-y-2 pt-10 md:pt-0 ">
+          <p>The Physics Multiverse </p>
+          <h1 className="text-4xl font-bold italic">EUREKA! ITB 2024</h1>
+          <h2 className="pt-10 text-2xl font-bold">COMPETITIONS</h2>
+          <Link
+            href="#"
+            className="hover:underline"
+          >
+            Physics Olympiad
+          </Link>
+          <Link
+            href="#"
+            className="hover:underline"
+          >
+            Scientific Writing Competition
+          </Link>
+          <h2 className="pt-10 text-2xl font-bold">ACKNOWLEDGEMENTS</h2>
+          <Link
+            href="#"
+            className="hover:underline"
+          >
+            Sponsors
+          </Link>
+          <Link
+            href="#"
+            className="hover:underline"
+          >
+            Media Partners
+          </Link>
         </div>
-      </footer>
-    </>
+      </div>
+      <div>
+        <div className="pt-5 sm:flex sm:items-center sm:justify-between">
+          <div className="flex">
+            <Link
+              href="https://www.instagram.com/eurekaitb/"
+              className="px-3 text-gray-400 hover:text-gray-200"
+            >
+              <BsInstagram />
+            </Link>
+            <div className="icon-instagram"></div>
+            <Link
+              href="https://twitter.com/eurekaitb"
+              className="px-3 text-gray-400 hover:text-gray-200"
+            >
+              <BsTwitterX />
+            </Link>
+            <Link
+              href="https://www.facebook.com/EurekaITB/"
+              className="px-3 text-gray-400 hover:text-gray-200"
+            >
+              <BsFacebook />
+            </Link>
+          </div>
+          <span className="text-sm text-gray-400 sm:text-center">
+            © 2024 EUREKA! ITB
+          </span>
+        </div>
+      </div>
+    </footer>
   );
 }
