13993902c7703d1b70eac0829c631afe3fa5ad67
ARoyyanF <160403210+ARoyyanF@users.noreply.github.com>
2024-05-16 13:33:11 +0700

    footer page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import FooterPage from "../app/footer/page";
+import Onboarding from "@/components/onboarding";
+import FooterPage from "../components/footer";
 
 export default function Home() {
   return (
-    <main className="flex size-full h-screen items-center justify-center gap-8">
-      Ini diisi sama coming soon
-      <FooterPage />
+    <main className="">
+      <div className="flex flex-col size-full h-screen items-center justify-center gap-8">Ini diisi sama coming soon</div>
+      {/* <Onboarding /> */}
+      <FooterPage />      
     </main>
   );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image'
+export default function FooterPage(){
+    return <>
+    
+<footer className="px-10 pb-10 w-full">
+    <div className="flex flex-col justify-between  md:flex-row md:items-center ">
+        <div className="flex flex-col items-center">
+            <Image src="./src/components/img/logo_eureka.svg" width={80} height={80} alt=""/>
+        </div>
+        <div className="flex flex-col items-end space-y-2 pt-10 md:pt-0 ">
+            <p>The Physics Multiverse </p>
+            <h1 className="text-4xl font-bold italic">EUREKA! ITB 2024</h1>
+            <h2 className="text-2xl font-bold pt-10">COMPETITIONS</h2>
+            <a href="#" className="hover:underline">Physics Olympiad</a>
+            <a href="#" className="hover:underline">Scientific Writing Competition</a> 
+            <h2 className="text-2xl font-bold pt-10">ACKNOWLEDGEMENTS</h2>
+            <a href="#" className="hover:underline">Sponsors</a> 
+            <a href="#" className="hover:underline">Media Partners</a> 
+        </div>
+    </div> 
+    <div>
+        <div className="sm:flex sm:items-center sm:justify-between pt-5">
+             <div className="flex">
+                <link href='https://unpkg.com/css.gg@2.0.0/icons/css/instagram.css' rel='stylesheet'/>
+                <link href='https://unpkg.com/css.gg@2.0.0/icons/css/facebook.css' rel='stylesheet'/>
+                <link href='https://unpkg.com/css.gg@2.0.0/icons/css/twitter.css' rel='stylesheet'/>
+                        
+                <a href="https://www.instagram.com/eurekaitb/" className="gg-instagram text-gray-500 hover:text-gray-900 dark:hover:text-white m-4"></a>
+                <a href="https://twitter.com/eurekaitb" className="gg-twitter text-gray-500 hover:text-gray-900 dark:hover:text-white m-4"></a>
+                <a href="https://www.facebook.com/EurekaITB/" className="gg-facebook text-gray-500 hover:text-gray-900 dark:hover:text-white m-4"></a>
+                
+            </div>
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 EUREKA! ITB.</span>
+        </div>
+    </div>
+</footer>
+
+    </>;
+}
