06520b89d8ab388c09267f43d7adaaaa7d471028
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2024-05-18 00:00:32 +0700

    feat: add infinite scroll animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -140,10 +140,18 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "horizontal-scroll": {
+          from: { transform: "translateX(-3rem)" },
+          to: { transform: "translateX(calc(-50% - 3rem))" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "infinite-horizontal-scroll": "horizontal-scroll 35s linear infinite",
+      },
+      backgroundImage: {
+        stars: "url('@/../public/stars.png')",
       },
     },
   },
