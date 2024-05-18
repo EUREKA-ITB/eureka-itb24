816d66a9ed1cf4b7a1dcacb9aadc8cbebae0e156
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2024-05-18 08:06:52 +0700

    fix: text too large and use vh instead of dvh

--- src/components/coming-soon-hero.tsx
+++ src/components/coming-soon-hero.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function ComingSoonHero() {
   return (
-    <section className="container flex h-[80dvh] w-full flex-col items-center justify-center gap-4 text-center lg:h-[75dvh]">
+    <section className="container flex h-[80vh] w-full flex-col items-center justify-center gap-4 text-center lg:h-[75vh]">
       <h3 className="text-3xl font-semibold md:text-4xl xl:text-6xl">
         EUREKA! ITB 2024
       </h3>
-      <h1 className="xs:text-8xl px-8 text-7xl font-bold drop-shadow-[0px_20px_64px_rgba(1,_170,_185,_1)] md:text-9xl 2xl:text-[10rem]">
+      <h1 className="px-8 text-7xl font-bold drop-shadow-[0px_20px_64px_rgba(1,_170,_185,_1)] sm:text-8xl md:text-9xl 2xl:text-[10rem]">
         COMING SOON
       </h1>
       <h3 className="text-2xl font-semibold md:text-3xl xl:text-5xl">
