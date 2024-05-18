f842a5ef73e4f6e0cadbd77b1693fb4b7f0d4e09
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2024-05-18 08:04:33 +0700

    fix: text too large on really small screen

--- src/components/coming-soon-hero.tsx
+++ src/components/coming-soon-hero.tsx
@@ -6,7 +6,7 @@ export default function ComingSoonHero() {
       <h3 className="text-3xl font-semibold md:text-4xl xl:text-6xl">
         EUREKA! ITB 2024
       </h3>
-      <h1 className="px-8 text-8xl font-bold drop-shadow-[0px_20px_64px_rgba(1,_170,_185,_1)] md:text-9xl 2xl:text-[10rem]">
+      <h1 className="xs:text-8xl px-8 text-7xl font-bold drop-shadow-[0px_20px_64px_rgba(1,_170,_185,_1)] md:text-9xl 2xl:text-[10rem]">
         COMING SOON
       </h1>
       <h3 className="text-2xl font-semibold md:text-3xl xl:text-5xl">
