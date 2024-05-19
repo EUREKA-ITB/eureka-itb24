a6b4c57f7e07711aaf546cc876dd256512a45e51
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2024-05-19 09:47:58 +0700

    chore: redundant dependecies cleanup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -143,15 +143,10 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
-        "horizontal-carousel": {
-          from: { transform: "translateX(calc(-25%))" },
-          to: { transform: "translateX(calc(25%))" },
-        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        "infinite-horizontal-scroll": "horizontal-carousel 35s linear infinite",
       },
       backgroundImage: {
         stars: "url('./../../public/stars.png')",
