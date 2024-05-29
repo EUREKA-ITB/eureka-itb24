251b62e8b361d66f24b4248b280cd52e9365c514
ARoyyanF <160403210+ARoyyanF@users.noreply.github.com>
2024-05-29 17:23:20 +0700

    rev 1 competition list items are made composable

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,6 +1,4 @@
-import img_phyOlympiad from "@/../public/img_phyOlympiad.svg";
-import img_sciWriting from "@/../public/img_sciWriting.svg";
-import CompetitionItems from "@/components/CompetitionItems";
+import { competitions } from "@/components/CompetitionItems";
 import { Badge } from "@/components/ui/badge";
 import Image from "next/image";
 
@@ -13,7 +11,35 @@ export default function Competition_List() {
           Things that are waiting beyond the horizon
         </h2>
       </div>
-      <CompetitionItems />
+      {competitions.map((competitions) => (
+        <div
+          key={null}
+          className="flex flex-row space-x-5"
+        >
+          <div className="my-auto hidden w-20 flex-shrink-0 sm:block">
+            <Image
+              src={competitions.image}
+              alt=""
+            />
+          </div>
+          <div className="flex flex-col space-y-2">
+            <div className="flex flex-col justify-between md:flex-row">
+              <div className="text-4xl font-bold">{competitions.name}</div>
+              <div className="my-auto flex-row space-x-4 space-y-4 md:space-y-0 ">
+                {competitions.levels.map((a) => (
+                  <Badge
+                    key={null}
+                    variant="default"
+                  >
+                    {a}
+                  </Badge>
+                ))}
+              </div>
+            </div>
+            <p>{competitions.description}</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
--- src/components/CompetitionItems.tsx
+++ src/components/CompetitionItems.tsx
@@ -1,80 +1,27 @@
 import img_phyOlympiad from "@/../public/img_phyOlympiad.svg";
 import img_sciWriting from "@/../public/img_sciWriting.svg";
-import { Badge } from "@/components/ui/badge";
-import Image from "next/image";
+import { StaticImageData } from "next/image";
 
-type TCompetitions = {
+export type TCompetitions = {
   name: string;
   levels: string[];
   description: string;
-  image: string;
+  image: StaticImageData;
 };
 
-const competitions: TCompetitions[] = [
+export const competitions: TCompetitions[] = [
   {
     name: "PHYSICS OLYMPIAD",
     levels: ["SMP/MTs", "SD/MI"],
     description:
       "We're no strangers to love You know the rules and so do I (do I) A full commitment's what I'm thinking of You wouldn't get this from any other guy I just wanna tell you how I'm feeling Gotta make you understand",
-    image: "sciWriting.svg",
+    image: img_sciWriting,
   },
   {
     name: "SCIENTIFIC WRITING",
     levels: ["SMP/MTs", "SD/MI"],
     description:
       "Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you",
-    image: "sciWriting.svg",
+    image: img_phyOlympiad,
   },
 ];
-export default function CompetitionItems() {
-  return (
-    <div className="flex flex-col space-y-10 px-20 xl:container xl:px-20">
-      <div className="flex flex-row space-x-5">
-        <div className="my-auto hidden w-20 flex-shrink-0 sm:block">
-          <Image
-            src={img_phyOlympiad}
-            alt=""
-          />
-        </div>
-        <div className="flex flex-col space-y-2">
-          <div className="flex flex-col justify-between md:flex-row">
-            <div className="text-4xl font-bold">PHYSICS OLYMPIAD </div>
-            <div className="my-auto flex-row space-x-4 space-y-4 md:space-y-0 ">
-              <Badge variant="default"> SMP/MTs</Badge>
-              <Badge variant="default">SD/MI</Badge>
-            </div>
-          </div>
-          <p>
-            We&apos;re no strangers to love You know the rules and so do I (do
-            I) A full commitment&apos;s what I&apos;m thinking of You
-            wouldn&apos;t get this from any other guy I just wanna tell you how
-            I&apos;m feeling Gotta make you understand
-          </p>
-        </div>
-      </div>
-      <div className="flex flex-row space-x-5">
-        <div className="my-auto hidden w-20 flex-shrink-0 sm:block">
-          <Image
-            src={img_sciWriting}
-            alt=""
-          />
-        </div>
-        <div className="flex flex-col space-y-2">
-          <div className="flex flex-col justify-between md:flex-row">
-            <div className="text-4xl font-bold">SCIENTIFIC WRITING </div>
-            <div className="my-auto flex-row space-x-4 space-y-4 md:space-y-0 ">
-              <Badge variant="default"> SMP/MTs</Badge>
-              <Badge variant="default">SD/MI</Badge>
-            </div>
-          </div>
-          <p>
-            We&apos;re no strangers to love You know the rules and so do I (do
-            I) A full commitment&apos;s what I&apos;m thinking of You
-            wouldn&apos;t get this from any other guy I just wanna tell you how
-            I&apos;m feeling Gotta make you understand
-          </p>
-        </div>
-      </div>
-    </div>
-  );
-}
