e9cf7cb27d707c452a8a561de3d3c428100badd7
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2024-07-02 02:30:30 +0800

    fix: prepare to fix missing images

--- src/app/(default)/_repositories/getGalleryImages.ts
+++ src/app/(default)/_repositories/getGalleryImages.ts
@@ -1,13 +1,9 @@
-import r1201 from "@public/1201.jpg";
-import labFidas from "@public/lab_fidas.jpg";
-import plawid from "@public/plawid.jpg";
-
 import type { TGalleryImage } from "../_models/gallery-interfaces";
 
 export default function getGalleryImages(): TGalleryImage[] {
   return [
-    { src: labFidas.src, alt: "Lab Fidas" },
-    { src: plawid.src, alt: "Plawid" },
-    { src: r1201.src, alt: "Ruangan 1201" },
+    { src: "/lab_fidas.jpg", alt: "Lab Fidas" },
+    { src: "/plawid.jpg", alt: "Plawid" },
+    { src: "/1201.jpg", alt: "Ruangan 1201" },
   ];
 }
