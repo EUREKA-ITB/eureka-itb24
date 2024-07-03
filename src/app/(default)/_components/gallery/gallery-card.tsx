3c7d229f333b8aa15b39cea929337a03eaefd589
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2024-07-03 15:34:29 +0800

    chore: improve responsiveness

--- src/app/(default)/_components/gallery/gallery-card.tsx
+++ src/app/(default)/_components/gallery/gallery-card.tsx
@@ -9,7 +9,7 @@ type TGalleryCardProps = {
 
 export default function GalleryCard(props: TGalleryCardProps) {
   return (
-    <Card className="group relative h-[15vh] w-[50rem] cursor-pointer overflow-hidden border-4 border-background transition-all ease-out md:aspect-[2/3] md:h-[55vh] md:w-[40rem] md:hover:-translate-y-4 md:hover:rotate-2 lg:hover:-translate-y-12">
+    <Card className="group relative h-[15vh] w-[50rem] cursor-pointer overflow-hidden border-4 border-background transition-all ease-out md:aspect-[2/3] md:h-[55vh] md:w-[45vw] md:min-w-[40rem] md:hover:-translate-y-4 md:hover:rotate-2 lg:hover:-translate-y-12">
       <Image
         src={props.image.src}
         alt={props.image.alt}
--- src/app/(default)/_components/hero/hero-text.tsx
+++ src/app/(default)/_components/hero/hero-text.tsx
@@ -14,7 +14,7 @@ export default function HeroText() {
         initial={{ translateY: 10, opacity: 0 }}
         animate={{ translateY: 0, opacity: 1 }}
         transition={{ duration: 0.35 }}
-        className="text-2xl font-light sm:text-3xl md:text-4xl xl:text-5xl"
+        className="text-2xl font-light sm:text-3xl md:text-4xl xl:text-5xl 2xl:text-7xl"
       >
         Explore and Experience
       </MotionH1>
@@ -23,7 +23,7 @@ export default function HeroText() {
         initial={{ translateY: 10, scale: 0.9, opacity: 0 }}
         animate={{ translateY: 0, scale: 1, opacity: 1 }}
         transition={{ duration: 0.3 }}
-        className="font-sans text-6xl font-bold tracking-tight drop-shadow-blue-glow sm:text-8xl lg:text-9xl xl:text-[9.5rem] xl:tracking-tighter"
+        className="font-sans text-6xl font-bold tracking-tight drop-shadow-blue-glow sm:text-8xl lg:text-9xl xl:text-[9.5rem] xl:tracking-tighter 2xl:text-[13rem]"
       >
         EUREKA! ITB
       </MotionH1>
@@ -32,7 +32,7 @@ export default function HeroText() {
         initial={{ translateY: -10, opacity: 0 }}
         animate={{ translateY: 0, opacity: 1 }}
         transition={{ duration: 0.4, delay: 0.3 }}
-        className="text-xl font-light sm:text-2xl md:text-3xl xl:text-4xl"
+        className="text-xl font-light sm:text-2xl md:text-3xl xl:text-4xl 2xl:text-6xl"
       >
         The Physics Multiverse
       </MotionH3>
