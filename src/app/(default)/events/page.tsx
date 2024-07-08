40b75270cd063bed262ce1a1e184ba1fac31dfdd
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2024-07-08 22:22:18 +0800

    chore: prepare for slicing events page

--- src/app/(default)/events/page.tsx
+++ src/app/(default)/events/page.tsx
@@ -1,3 +1,7 @@
 export default function EventsPage() {
-  return <div>EventsPage</div>;
+  return (
+    <div className="container flex flex-col py-6">
+      <h1 className="text-6xl font-bold drop-shadow-blue-glow">EVENTS</h1>
+    </div>
+  );
 }
