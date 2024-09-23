678ed0e993fdf91fed387499e1ddf87b4ccd7a20
rdanielsn <10221010@mahasiswa.itb.ac.id>
2024-09-23 15:13:08 +0700

    feat: add olympiad elimination on navbar

--- src/lib/repositories/getNavigationBarMenu.tsx
+++ src/lib/repositories/getNavigationBarMenu.tsx
@@ -3,6 +3,11 @@ import { AiFillBook } from "react-icons/ai";
 export default function getNavigationBarMenu() {
   return [
     {
+      label: "Olympiad Elimination",
+      href: "https://eurekaitb.com", // Attention: change this to the correct URL later
+      icon: <AiFillBook size={16} />,
+    },
+    {
       label: "Abstract Selection",
       href: "https://bit.ly/HasilSeleksiAbstrakFinalEUREKAITB2024",
       icon: <AiFillBook size={16} />,
